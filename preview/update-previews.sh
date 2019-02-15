9ac64fca332ee34e08552afbe5e371c979289598
Alex Szczuczko <alex@szc.ca>
2019-02-15 16:42:49 -0700

    Make previews smaller and add white background

--- preview/update-previews.sh
+++ preview/update-previews.sh
@@ -2,8 +2,36 @@
 
 set -euo pipefail
 
-cd render
-for i in {1..4}
-do
-    pdf2svg "../biab.pdf" "biab-$i.svg" "$i"
-done
+cd preview
+
+render() {
+    name="$1"
+    pages="$2"
+    echo "==> $name" >&2
+    for i in $(seq 1 "$pages")
+    do
+        echo " -> $i" >&2
+        out="$name-$i.svg"
+        tmp="$(mktemp XXXXXXXXXX.tmp.svg)"
+        # Convert
+        pdf2svg "../$name.pdf" "$out" "$i"
+
+        # Add white background
+        xmlstarlet ed \
+            -L \
+            -N "svg=http://www.w3.org/2000/svg" \
+            -i '/svg:svg/*[1]' -t elem -n "rect" \
+            -s '//rect' -t attr -n "width" -v "100%" \
+            -s '//rect' -t attr -n "height" -v "100%" \
+            -s '//rect' -t attr -n "fill" -v "white" \
+            "$out"
+
+        # Clean / Minify
+        svgcleaner --quiet --multipass --indent 1 "$out" "$tmp"
+        mv "$tmp" "$out"
+    done
+}
+
+render biab 4
+
+echo "==> Done" >&2
